41fc323f720d73c1ac02d81694f6af5be12d33b0
Kenzie Tama <kenzietama@students.undip.ac.id>
2025-09-16 21:14:16 +0700

    update

--- echoclient.c
+++ echoclient.c
@@ -1,34 +1,58 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
-int main(int argc, char **argv)
-{
+#define PORT 1313
+
+int main() {
     int sockfd, n;
-    char sendline[100];
-    char recvline[100];
-    struct sockaddr_in servaddr;
+    struct sockaddr_in serv_addr;
+    char buffer[1024];
+    char message[1024];
 
+    // Create socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    bzero(&servaddr, sizeof servaddr);
-
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(22000);
-
-    inet_pton(AF_INET, "172.16.1.1", &(servaddr.sin_addr));
-
-    connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
-
-    while (1)
-    {
-        bzero(sendline, 100);
-        bzero(recvline, 100);
-        fgets(sendline, 100, stdin);
-
-        write(sockfd, sendline, strlen(sendline) + 1);
-        read(sockfd, recvline, 100);
-        printf("%s", recvline);
+    if (sockfd < 0) {
+        perror("ERROR opening socket");
+        exit(1);
     }
+
+    // Configure server address
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    if (inet_pton(AF_INET, "192.168.235.251", &serv_addr.sin_addr) <= 0) {
+        perror("Invalid address/Address not supported");
+        exit(1);
+    }
+
+    // Connect to server
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection Failed");
+        exit(1);
+    }
+    printf("Connected to echo server. Type messages (CTRL+C to exit):\n");
+
+    // Loop to send messages to server and receive echo
+    while (1) {
+        printf("> ");
+        fgets(message, sizeof(message), stdin);
+        n = write(sockfd, message, strlen(message));
+        if (n < 0) {
+            perror("ERROR writing to socket");
+            break;
+        }
+        memset(buffer, 0, sizeof(buffer));
+        n = read(sockfd, buffer, 1023);
+        if (n <= 0) {
+            printf("Server disconnected.\n");
+            break;
+        }
+        printf("Echo: %s", buffer);
+    }
+
+    close(sockfd);
+    return 0;
 }
--- echoserver.c
+++ echoserver.c
@@ -1,40 +1,68 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
-int main()
-{
+#define PORT 1313
 
-    char str[100];
-    int listen_fd, comm_fd;
+int main() {
+    int sockfd, newsockfd;
+    socklen_t clilen;
+    char buffer[1024];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
 
-    struct sockaddr_in servaddr;
-
-    listen_fd = socket(AF_INET, SOCK_STREAM, 0);
-
-    bzero(&servaddr, sizeof(servaddr));
-
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htons(INADDR_ANY);
-    servaddr.sin_port = htons(22000);
-
-    bind(listen_fd, (struct sockaddr *)&servaddr, sizeof(servaddr));
-
-    listen(listen_fd, 10);
-
-    comm_fd = accept(listen_fd, (struct sockaddr *)NULL, NULL);
-
-    while (1)
-    {
-
-        bzero(str, 100);
-
-        read(comm_fd, str, 100);
-
-        printf("Echoing back - %s", str);
-
-        write(comm_fd, str, strlen(str) + 1);
+    // Create socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("ERROR opening socket");
+        exit(1);
     }
+
+    // Initialize server address structure
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(PORT);
+
+    // Bind socket to address
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        perror("ERROR on binding");
+        exit(1);
+    }
+
+    // Listen for connections
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
+    printf("Echo server listening on port %d...\n", PORT);
+
+    // Main loop to accept client connections
+    while (1) {
+        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+        if (newsockfd < 0) {
+            perror("ERROR on accept");
+            continue;
+        }
+        printf("Client connected.\n");
+
+        // Loop to read and echo client messages
+        while (1) {
+            memset(buffer, 0, 1024);
+            n = read(newsockfd, buffer, 1023);
+            if (n <= 0) {
+                printf("Client disconnected.\n");
+                close(newsockfd);
+                break;
+            }
+            printf("Received: %s", buffer);
+            n = write(newsockfd, buffer, strlen(buffer));
+            if (n < 0) {
+                perror("ERROR writing to socket");
+            }
+        }
+    }
+
+    close(sockfd);
+    return 0;
 }
